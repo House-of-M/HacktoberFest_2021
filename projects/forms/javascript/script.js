a7c2465dededd414b34e69cb778b04432a59f2d6
L4RBI <adelllarbi@gmail.com>
2021-10-27 00:04:57 +0100

    added a form demo that i use to teach events

---
+++ projects/forms/javascript/script.js
@@ -0,0 +1,34 @@
+document.querySelector("form").addEventListener("submit", function (e) {
+    e.preventDefault();//stop le comportement par defaut
+
+    if (document.getElementById("js-password-input").value.length < 10) {
+        document.getElementById('passwordHelp').innerText = "your password needs to be 10 characters or longer.";
+    } else {
+        document.getElementById('passwordHelp').innerText = "";
+    }
+    if (document.getElementById("firstName").value == "") {
+        document.querySelector("#firstNameHelp").innerText = "you have to fill-in your first name.";
+    } else {
+        document.querySelector("#firstNameHelp").innerText = "";
+    }
+    if (document.getElementById("lastName").value == "") {
+        document.querySelector("#lastNameHelp").innerText = "you have to fill-in your last name.";
+    } else {
+        document.querySelector("#lastNameHelp").innerText = "";
+    }
+    if (document.getElementById("js-email-input").value == "") {
+        document.querySelector("#emailHelp").innerText = "you have to fill-in your email.";
+    } else {
+        document.querySelector("#emailHelp").innerText = "";
+    }
+    if (document.getElementById("js-checkbox").checked) {
+        document.querySelector("#checkHelp").innerText = "";
+    } else {
+        document.querySelector("#checkHelp").innerText = "please check here.";
+    }
+    if (document.getElementById("js-password-input").value.length >= 10 && document.getElementById("firstName").value != "" && document.getElementById("lastName").value != "" && document.getElementById("js-email-input").value != "" && document.getElementById("js-checkbox").checked) {
+        e.target.setAttribute('hidden', '');
+        document.querySelector("h1").removeAttribute('hidden');
+    }
+
+});

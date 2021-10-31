3a94641d0ae8874be44f38821a5f25723ff6d0a7
dimitrisz123 <dimitris-007@hotmail.com>
2021-10-31 22:42:39 +0200

    Check for URL in a String

---
+++ projects/Check for URL in a String/Check for URL in a String.py
@@ -0,0 +1,13 @@
+import re
+  
+def Find(string):
+  
+    # findall() has been used 
+    # with valid conditions for urls in string
+    regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+    url = re.findall(regex,string)      
+    return [x[0] for x in url]
+      
+# Driver Code
+string = 'My Profile: https://auth.geeksforgeeks.org/user/Chinmoy%20Lenka/articles in the portal of http://www.geeksforgeeks.org/'
+print("Urls: ", Find(string))
